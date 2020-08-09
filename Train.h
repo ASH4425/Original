40fcded7c6f8c8d4140d79d0bdf06564eea639b4
소현 안 <thgus4425@naver.com>
2020-08-09 18:13:50 +0900

    Update

--- Train.h
+++ Train.h
@@ -43,6 +43,7 @@ extern double totalWeightUpdate; // track the total weight update (absolute valu
 extern double totalNumPulse;// track the total number of pulse for the weight update process; for Analog device only
 // void Train(const int numTrain, const int epochs);
 void Train(const int numTrain, const int epochs, char* optimization_type); // For decayed learning rate
+int ReturnbatchSize();
 /* Availiable optimization type includes
 "SGD": the stochastic gradient descent
 "Momentum": the momentum optimization
