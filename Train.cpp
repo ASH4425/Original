bcf8b7e24ed62537731341f5a528b920217a4223
Cell_IdealPCM_Pseudo <thgus4425@naver.com>
2020-08-12 18:09:24 +0900

    Delete WT

--- Train.cpp
+++ Train.cpp
@@ -1056,7 +1056,7 @@ double s2[param->nOutput];  // Output delta from hidden layer to the output laye
 
 
 //start weight tracking
-		char *fileIH;
+		/*char *fileIH;
 		char *fileHO;
 
 				if (finalbatch) {
@@ -1124,7 +1124,7 @@ double s2[param->nOutput];  // Output delta from hidden layer to the output laye
 					}
 				}
 		// end of weight tracking code
-				
+				*/
 		}
     }
 }
