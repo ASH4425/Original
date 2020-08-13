731cf820c1caab38201f732c240d15482719a3c4
Cell_IdealPCM_Pseudo <thgus4425@naver.com>
2020-08-13 14:01:49 +0900

    View WaitTime and numPulse

--- Array.cpp
+++ Array.cpp
@@ -84,7 +84,7 @@ double Array::ReadCell(int x, int y, char* mode) {
 					
 			if (x == 1 && y == 1) {
 				//std::cout << static_cast<eNVM*>(cell[0][0])->batchSizeZero << std::endl;
-				std::cout << static_cast<eNVM*>(cell[x][y])->waitTime;
+				std::cout << static_cast<eNVM*>(cell[x][y])->waitTime << "  ";
 				std::cout << static_cast<AnalogNVM*>(cell[x][y])->numPulse << std::endl;
 			}
 			
