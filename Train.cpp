2adbc59f87d39f8b56d2875a049e045b13be9157
Cell_IdealPCM_Pseudo <thgus4425@naver.com>
2020-08-13 14:24:17 +0900

    View WaitTime and numPulse

--- Train.cpp
+++ Train.cpp
@@ -1084,16 +1084,16 @@ double s2[param->nOutput];  // Output delta from hidden layer to the output laye
 			for (int bi = 0; bi < 100; bi++) {
 				for (int bj = 0; bj < 400; bj++) {
 					if (static_cast<AnalogNVM*>(arrayIH->cell[bi][bj])->numPulse != 0) { 
-						notZeroi = bi; 
-						notZeroj = bj;
+						//notZeroi = bi; 
+						//notZeroj = bj;
 						std::cout << bi << " " << bj << std::endl;
 					}
 				}
 			}
 
 			//std::cout << static_cast<eNVM*>(cell[0][0])->batchSizeZero << std::endl;
-			std::cout << static_cast<eNVM*>(arrayIH->cell[notZeroi][notZeroj])->waitTime << "  ";
-			std::cout << static_cast<AnalogNVM*>(arrayIH->cell[notZeroi][notZeroj])->numPulse << std::endl;
+			//std::cout << static_cast<eNVM*>(arrayIH->cell[notZeroi][notZeroj])->waitTime << "  ";
+			//std::cout << static_cast<AnalogNVM*>(arrayIH->cell[notZeroi][notZeroj])->numPulse << std::endl;
 
 
 
